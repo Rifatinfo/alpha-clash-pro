f1e4d6db49290b66ae398864fb08fe13e6034bcf
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-02-28 12:54:35 +0600

    key press connected by javascript

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,15 +1,29 @@
-function play(){
-    //hide the home screen 
-     const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add("hidden");
-     // console.log(homeSection.classList)
+// function play(){
+//     //hide the home screen 
+//      const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add("hidden");
+//      // console.log(homeSection.classList)
 
-    // show the playground 
-    const playgroundSection = document.getElementById('play-ground ');
-    //console.log(playgroundSection.classList)
-    playgroundSection.classList.remove('hidden');
+//     // show the playground 
+//     const playgroundSection = document.getElementById('play-ground');
+//     //console.log(playgroundSection.classList)
+//     playgroundSection.classList.remove("hidden");
 
-    continueGame();
-
+// }
+function continueGame(){
+    // step-1: generate a random alphabet 
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
 }
 
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
+
+
+
+ 
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,2 +1,29 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function getARandomAlphabet(){
+    // get or create an alphabet array 
+    const  alphabetString = 'qwertyuioplkjhgfdsazxcvbnm';
+    const alphabets = alphabetString.split('');
+   // console.log(alphabet);
+
+    // get a random index between 0-25 
+    const randomNumber = Math.random()*25;
+    console.log(randomNumber); // 21.17765
+    const index = Math.round(randomNumber)
+   console.log(index); // 21 
+
+    const alphabet = alphabets[index];
+    //console.log(index, alphabet);
+    return alphabet ;
+
+}
 
 
